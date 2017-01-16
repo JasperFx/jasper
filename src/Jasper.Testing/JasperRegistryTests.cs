96aca4386814def1f93cb2aeb12741885558d010
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-01-16 08:59:01 -0600

    JasperRegistry can find it's application assembly

---
+++ src/Jasper.Testing/JasperRegistryTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Shouldly;
+using Xunit;
+
+namespace Jasper.Testing
+{
+    public class JasperRegistryTests
+    {
+        [Fact]
+        public void can_determine_the_root_assembly_on_subclass()
+        {
+            new MyRegistry().ApplicationAssembly.ShouldBe(typeof(JasperRegistryTests).GetTypeInfo().Assembly);
+        }
+
+        public class MyRegistry : JasperRegistry
+        {
+            
+        }
+    }
+}
--- src/Jasper/JasperRegistry.cs
+++ src/Jasper/JasperRegistry.cs
@@ -1,9 +1,22 @@
-﻿using Jasper.Configuration;
+﻿using System.Reflection;
+using Jasper.Configuration;
+using StructureMap.TypeRules;
 
 namespace Jasper
 {
     public class JasperRegistry
     {
+        public JasperRegistry()
+        {
+            var assembly = this.GetType().GetAssembly();
+            if (assembly != typeof(JasperRegistry).GetAssembly())
+            {
+                ApplicationAssembly = assembly;
+            }
+        }
+
+        public Assembly ApplicationAssembly { get; private set; }
+
         public ServiceRegistry Services { get; } = new ServiceRegistry();
     }
 }
