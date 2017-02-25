8e26790bf2c3df2ab7b450d76b0b6d23c1caa83c
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-02-25 07:46:04 -0600

    added IDataSource

--- src/Jasper/Binding/IDataSource.cs
+++ src/Jasper/Binding/IDataSource.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Configuration;
 
 namespace Jasper.Binding
 {
@@ -7,10 +9,44 @@ namespace Jasper.Binding
         bool Has(string key);
         string Get(string key);
 
-        IEnumerable<string> Keys();
-
         bool HasChild(string key);
 
         IDataSource GetChild(string key);
+    }
+
+    public class ConfigurationProviderDataSource : IDataSource
+    {
+        private readonly IConfigurationProvider _provider;
+
+        public ConfigurationProviderDataSource(IConfigurationProvider provider)
+        {
+            _provider = provider;
+            provider.Load();
+        }
+
+        public bool Has(string key)
+        {
+            string value = null;
+            return _provider.TryGet(key, out value);
+        }
+
+        public string Get(string key)
+        {
+            string value = null;
+            _provider.TryGet(key, out value);
+
+            return value;
+        }
+
+        public bool HasChild(string key)
+        {
+            return _provider.
+            throw new System.NotImplementedException();
+        }
+
+        public IDataSource GetChild(string key)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
