56636ddae4882cfceb3fdf69da858311421f90eb
jeremydmiller <jeremydmiller@yahoo.com>
2017-02-27 08:00:53 -0600

    added InvocationContext from fubu

---
+++ src/JasperBus.Tests/Runtime/Invocation/InvocationContextTester.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using JasperBus.Model;
+using JasperBus.Runtime;
+using JasperBus.Runtime.Invocation;
+using Shouldly;
+using Xunit;
+
+namespace JasperBus.Tests.Runtime.Invocation
+{
+
+    public class InvocationContextTester
+    {
+
+        [Fact]
+        public void enqueue()
+        {
+            var messages = new InvocationContext(new Envelope{Message = new Message1()}, new HandlerChain());
+            var m1 = new Message1();
+            var m2 = new Message2();
+
+            messages.EnqueueCascading(m1);
+            messages.EnqueueCascading(m2);
+
+            messages.OutgoingMessages().ShouldHaveTheSameElementsAs(m1, m2);
+        }
+
+        [Fact]
+        public void ignores_nulls_just_fine()
+        {
+            var messages = new InvocationContext(new Envelope { Message = new Message1() }, new HandlerChain());
+            messages.EnqueueCascading(null);
+
+            messages.OutgoingMessages().Any().ShouldBeFalse();
+        }
+
+        [Fact]
+        public void enqueue_an_oject_array()
+        {
+            var messages = new InvocationContext(new Envelope{Message = new Message1()}, new HandlerChain());
+            var m1 = new Message1();
+            var m2 = new Message2();
+
+            messages.EnqueueCascading(new object[]{m1, m2});
+
+            messages.OutgoingMessages().ShouldHaveTheSameElementsAs(m1, m2);
+        }
+    }
+
+
+}
---
+++ src/JasperBus/Model/HandlerChain.cs
@@ -0,0 +1,7 @@
+﻿namespace JasperBus.Model
+{
+    public class HandlerChain
+    {
+
+    }
+}
