c618652e7d3e3279c760b603a280b92c8af05953
jeremydmiller <jeremydmiller@yahoo.com>
2017-03-09 18:19:11 -0600

    missed methods on ITransport

--- src/JasperBus/Runtime/ITransport.cs
+++ src/JasperBus/Runtime/ITransport.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using JasperBus.Configuration;
 
 namespace JasperBus.Runtime
 {
@@ -6,5 +7,26 @@ namespace JasperBus.Runtime
     {
         string Protocol { get; }
 
+        /// <summary>
+        /// Creates a channel for a known channel node upfront
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        IChannel CreateChannel(ChannelNode node);
+
+
+        /// <summary>
+        /// Creates a functional node for a channel to the reply Uri of an incoming message
+        /// </summary>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        IChannel CreateDestinationChannel(Uri destination);
+
+        /// <summary>
+        /// An address that can be used as the "callback" Uri for messages
+        /// sent to a remote node
+        /// </summary>
+        /// <returns></returns>
+        Uri ReplyUri();
     }
 }
