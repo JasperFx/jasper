f81f5278850df66b21ce4661f65c23a8ee5674a2
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-03-13 17:50:08 -0500

    stubbed in subscription classes

---
+++ src/JasperBus.Tests/Runtime/Subscriptions/Subscription_matches_tester.cs
@@ -0,0 +1,18 @@
+﻿using JasperBus.Runtime.Subscriptions;
+using Shouldly;
+using Xunit;
+
+namespace JasperBus.Tests.Runtime.Subscriptions
+{
+    public class Subscription_matches_tester
+    {
+        [Fact]
+        public void fuzzy_matches()
+        {
+            var subscription = Subscription.For<Message1>();
+
+            subscription.Matches(typeof(Message1)).ShouldBeTrue();
+            subscription.Matches(typeof(Message2)).ShouldBeFalse();
+        }
+    }
+}
