83d31dbf31362cc4590c47c2570dcee3a8190a9d
Joe McBride <joe@uicraftsman.com>
2017-03-15 12:02:28 -0700

    Redux

--- src/Jasper.Diagnostics/app/index.js
+++ src/Jasper.Diagnostics/app/index.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter } from 'react-router'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+import rootReducer from './rootReducer';
 import App from './App'
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
 
 const settings = window.DiagnosticsSettings;
 
 render(
   <HashRouter>
-    <App settings={settings}/>
+    <Provider store={store}>
+      <App settings={settings}/>
+    </Provider>
   </HashRouter>,
   document.getElementById('root')
 )
