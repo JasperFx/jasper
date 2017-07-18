33d7151197ff6f9a20276f959c106b5f8170bc71
jeremydmiller <jeremydmiller@yahoo.com>
2017-07-17 19:16:15 -0500

    Converted Jasper.Consul over to using the Consul lib as is

--- src/Jasper.Consul/ConsulGateway.cs
+++ src/Jasper.Consul/ConsulGateway.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Consul;
 using Jasper.Bus.Runtime;
 using Newtonsoft.Json;
 
@@ -10,78 +11,37 @@ namespace Jasper.Consul
     // TODO -- clean this code up and eliminate the duplication. It's at spike quality code at the moment
     public interface IConsulGateway
     {
-        Task RegisterService(ServiceRegistration registration);
-        Task<ServiceRegistration[]> GetRegisteredServices();
-        Task<ServiceRegistration[]> GetRegisteredServices(string serviceName);
         Task<string> GetProperty(string key);
         Task SetProperty(string key, string value);
-        Task Register(RunningService service);
-        Task UnRegister(string id);
     }
 
     public class ConsulGateway : IConsulGateway
     {
         private readonly HttpClient _client;
         private readonly ConsulSettings _settings;
+        private readonly ConsulClient _consul;
 
         public ConsulGateway(HttpClient client, ConsulSettings settings)
         {
             _client = client;
             _settings = settings;
+            _consul = new ConsulClient(settings.Configure);
         }
 
-        public Task RegisterService(ServiceRegistration registration)
-        {
-            var json = JsonConvert.SerializeObject(registration);
-            var bytes = Encoding.UTF8.GetBytes(json);
-            var content = new ByteArrayContent(bytes);
-
-            return _client.PutAsync($"http://localhost:{_settings.Port}/v1/catalog/register",
-                content);
-        }
-
-        public async Task<ServiceRegistration[]> GetRegisteredServices()
-        {
-            var json = await _client.GetStringAsync($"http://localhost:{_settings.Port}/v1/catalog/nodes");
-            var raw = JsonConvert.DeserializeObject<ServiceRegistration[]>(json);
-            return raw.Where(x => x.ServiceName != ServiceRegistration.Unknown).ToArray();
-        }
-
-        public async Task<ServiceRegistration[]> GetRegisteredServices(string serviceName)
-        {
-            var json = await _client.GetStringAsync($"http://localhost:{_settings.Port}/v1/catalog/nodes/{serviceName}");
-            var raw = JsonConvert.DeserializeObject<ServiceRegistration[]>(json);
-            return raw.Where(x => x.ServiceName != ServiceRegistration.Unknown).ToArray();
-        }
 
         public async Task<string> GetProperty(string key)
         {
-            var json = await _client.GetStringAsync($"http://localhost:{_settings.Port}/v1/kv/{key}");
-            var response = JsonConvert.DeserializeObject<KeyValueResponse[]>(json);
-
-            return Encoding.UTF8.GetString(response.First().Value);
+            var result = await _consul.KV.Get(key);
+            var data = result.Response.Value;
+            return Encoding.UTF8.GetString(data);
         }
 
         public Task SetProperty(string key, string value)
         {
-            var content = new StringContent(value);
-            return _client.PutAsync($"http://localhost:{_settings.Port}/v1/kv/{key}", content);
-        }
-
-        public Task Register(RunningService service)
-        {
-            var json = JsonConvert.SerializeObject(service);
-            var bytes = Encoding.UTF8.GetBytes(json);
-            var content = new ByteArrayContent(bytes);
-
-            return _client.PutAsync($"http://localhost:{_settings.Port}/v1/agent/service/register",
-                    content);
-        }
-
-        public Task UnRegister(string id)
-        {
-            return _client.PutAsync($"http://localhost:{_settings.Port}/v1/agent/service/deregister/{id}",
-                new StringContent(id));
+            return _consul.KV.Put(new KVPair(key)
+            {
+                Value = Encoding.UTF8.GetBytes(value)
+            });
         }
 
     }
--- src/Jasper.Consul/ConsulSettings.cs
+++ src/Jasper.Consul/ConsulSettings.cs
@@ -1,7 +1,15 @@
-﻿namespace Jasper.Consul
+﻿using Consul;
+using Jasper.Bus.Runtime;
+
+namespace Jasper.Consul
 {
     public class ConsulSettings
     {
         public int Port { get; set; } = 8500;
+
+        internal void Configure(ConsulClientConfiguration config)
+        {
+            config.Address = $"http://localhost:{Port}".ToUri();
+        }
     }
 }
