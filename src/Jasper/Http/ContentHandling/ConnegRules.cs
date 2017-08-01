c1e69b768a9bf221df001ad30ce08b9df26e392a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-08-01 09:14:05 -0500

    Spiked in the default conneg json in/json out mechanism

--- src/Jasper/Http/ContentHandling/ConnegRules.cs
+++ src/Jasper/Http/ContentHandling/ConnegRules.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using Baseline;
+using Jasper.Codegen;
 using Jasper.Conneg;
 using Jasper.Http.Model;
 using Jasper.Http.Routing;
@@ -28,24 +29,27 @@ namespace Jasper.Http.ContentHandling
             if (chain.InputType != null)
             {
                 var rule = _readers.FirstOrDefault(x => x.Applies(chain));
-                if (rule == null)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(chain), $"Unable to determine a reader strategy for route {chain}");
-                }
-
-                chain.Middleware.AddRange(rule.DetermineReaders(chain));
+                var frames = rule?.DetermineReaders(chain) ?? jsonReadersFor(chain);
+                chain.Middleware.AddRange(frames);
             }
 
             if (chain.ResourceType != null)
             {
                 var rule = _writers.FirstOrDefault(x => x.Applies(chain));
-                if (rule == null)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(chain), $"Unable to determine a writer strategy for route {chain}");
-                }
+                var frames = rule?.DetermineWriters(chain) ?? jsonWritersFor(chain);
 
-                chain.Postprocessors.AddRange(rule.DetermineWriters(chain));
+                chain.Postprocessors.AddRange(frames);
             }
+        }
+
+        private IEnumerable<Frame> jsonReadersFor(RouteChain chain)
+        {
+            yield break;
+        }
+
+        private IEnumerable<Frame> jsonWritersFor(RouteChain chain)
+        {
+            yield break;
         }
     }
 }
