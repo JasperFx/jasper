746442107219bd65b9ee87069758e9c4e5fcb150
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-08-08 07:23:49 -0500

    Sample console app

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Jasper;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JasperAgent.Run<ConsoleAppRegistry>();
+        }
+    }
+
+    public class ConsoleAppRegistry : JasperRegistry
+    {
+        public ConsoleAppRegistry()
+        {
+            Http.UseKestrel().UseUrls("http://localhost:3001");
+
+            Channels.ListenForMessagesFrom("jasper://localhost:2222/incoming");
+        }
+    }
+}
