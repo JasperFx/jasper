f4b74280f0ee38d6ec3f631e63ba0bb2371a1b1b
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-09-13 16:08:42 -0500

    additional content for error handling docs

---
+++ src/Jasper/Bus/Runtime/Invocation/DelayedResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Jasper.Bus.Runtime.Invocation
+{
+    public class DelayedResponse : ISendMyself
+    {
+        private readonly object _outgoing;
+
+        public DelayedResponse(object outgoing, TimeSpan delay) : this(outgoing, DateTime.UtcNow.Add(delay))
+        {
+            Delay = delay;
+        }
+
+        public DelayedResponse(object outgoing, DateTime time)
+        {
+            _outgoing = outgoing;
+            Time = time;
+        }
+
+        public object Outgoing => _outgoing;
+
+        public DateTime Time { get; }
+
+        public TimeSpan Delay { get; }
+
+        public Envelope CreateEnvelope(Envelope original)
+        {
+            var outgoing = original.ForResponse(_outgoing);
+            outgoing.ExecutionTime = Time;
+
+            return outgoing;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Execute {0} at time: {1}", _outgoing, Time);
+        }
+    }
+}
