b452149272a6efc05ed4c64fa31a7ad88db0d52d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-09-27 23:17:22 -0500

    wip: BuildStepPlanner work

---
+++ src/BlueMilk.Tests/IoC/BuildStepPlannerTester.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using BlueMilk.Codegen;
+using BlueMilk.Compilation;
+using BlueMilk.IoC;
+using BlueMilk.Tests.TargetTypes;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace BlueMilk.Tests.IoC
+{
+    public class CommentFrame : SyncFrame
+    {
+        public override void GenerateCode(GeneratedMethod method, ISourceWriter writer)
+        {
+            writer.Write("// Kilroy was here.");
+            Next?.GenerateCode(method, writer);
+        }
+    }
+
+    public class BuildStepPlannerTester
+    {
+        public readonly ServiceRegistry theServices = new ServiceRegistry();
+        private ServiceGraph theGraph;
+        private GeneratedMethod theMethod;
+
+        public BuildStepPlannerTester()
+        {
+            theGraph = new ServiceGraph(theServices);
+            theMethod = new GeneratedMethod("Go", new Argument[0], new List<Frame>{new CommentFrame()});
+
+
+        }
+
+        private BuildStepPlanner executePlan<T>()
+        {
+            return new BuildStepPlanner(typeof(T), theGraph, theMethod);
+        }
+
+        [Fact]
+        public void not_reduceable_if_no_public_ctors()
+        {
+            executePlan<NoPublicCtorGuy>()
+                .CanBeReduced.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void not_reduceable_if_cannot_resolve_all()
+        {
+            executePlan<WidgetUsingGuy>()
+                .CanBeReduced.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void reduceable_if_can_be_built_with_services()
+        {
+            theServices.AddTransient<IWidget, AWidget>();
+
+            executePlan<WidgetUsingGuy>()
+                .CanBeReduced.ShouldBeTrue();
+        }
+
+    }
+
+    public class WidgetUsingGuy
+    {
+        public WidgetUsingGuy(IWidget widget)
+        {
+        }
+    }
+
+    public class NoPublicCtorGuy
+    {
+        private NoPublicCtorGuy()
+        {
+        }
+    }
+}
---
+++ src/BlueMilk/IoC/ConstructorFrame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BlueMilk.Codegen;
+using BlueMilk.Compilation;
+using BlueMilk.Util;
+
+namespace BlueMilk.IoC
+{
+    public class ConstructorFrame : SyncFrame
+    {
+        private readonly Variable[] _arguments;
+        public Type ServiceType { get; }
+        public Type ImplementationType { get; }
+        public Variable Variable { get; }
+
+        public ConstructorFrame(Type serviceType, Type implementationType, string variableName, Variable[] arguments)
+        {
+            _arguments = arguments;
+            ServiceType = serviceType;
+            ImplementationType = implementationType;
+            Variable = new Variable(serviceType, variableName, this);
+        }
+
+        public int Number { get; set; }
+
+        public override void GenerateCode(GeneratedMethod method, ISourceWriter writer)
+        {
+            var arguments = _arguments.Select(x => x.Usage).Join(", ");
+            var implementationTypeName = ImplementationType.FullName.Replace("+", ".");
+            writer.Write($"var {Variable.Usage} = new {implementationTypeName}({arguments});");
+            Next?.GenerateCode(method, writer);
+        }
+
+        public override IEnumerable<Variable> FindVariables(GeneratedMethod chain)
+        {
+            return _arguments;
+        }
+    }
+}
