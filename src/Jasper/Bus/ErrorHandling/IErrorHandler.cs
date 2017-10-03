16d9361986789e39eaa64e228354f7b9aec2b3af
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-10-03 16:49:58 -0500

    More Xml API comments

--- src/Jasper/Bus/ErrorHandling/IErrorHandler.cs
+++ src/Jasper/Bus/ErrorHandling/IErrorHandler.cs
@@ -5,6 +5,10 @@ using Jasper.Bus.Runtime.Invocation;
 namespace Jasper.Bus.ErrorHandling
 {
     // SAMPLE: IErrorHandler
+    /// <summary>
+    /// Potentially choose a continuation that would determine what happens next for a message
+    /// that encountered the given exception at processing time
+    /// </summary>
     public interface IErrorHandler
     {
         IContinuation DetermineContinuation(Envelope envelope, Exception ex);
