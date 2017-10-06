9d4823ba88a8dfbe48e8360d151ddc5ea8bffb5a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-10-06 10:00:33 -0500

    ping pong samples

---
+++ src/Pinger/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Jasper.CommandLine;
+using Oakton;
+using TestMessages;
+
+namespace Pinger
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return JasperAgent.Run(args, _ =>
+            {
+                _.Logging.UseConsoleLogging = true;
+
+                _.Transports.Lightweight.ListenOnPort(2600);
+
+                // Using static routing rules to start
+                _.Publish.Message<PingMessage>().To("tcp://localhost:2601");
+            });
+        }
+    }
+
+    public class PongHandler
+    {
+        public void Handle(PongMessage message)
+        {
+            ConsoleWriter.Write(ConsoleColor.Cyan, "Got a pong back with name: " + message.Name);
+
+        }
+    }
+}
---
+++ src/Ponger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Jasper.Bus.Runtime.Invocation;
+using Jasper.CommandLine;
+using Oakton;
+using TestMessages;
+
+namespace Ponger
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return JasperAgent.Run(args, _ =>
+            {
+                _.Logging.UseConsoleLogging = true;
+
+                _.Transports.Lightweight.ListenOnPort(2601);
+            });
+        }
+    }
+
+    public class PingHandler
+    {
+        public object Handle(PingMessage message)
+        {
+            ConsoleWriter.Write(ConsoleColor.Cyan, "Got a ping with name: " + message.Name);
+
+            var response = new PongMessage
+            {
+                Name = message.Name
+            };
+
+            return Respond.With(response).ToSender();
+        }
+    }
+}
