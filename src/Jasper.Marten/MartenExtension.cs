cc07c698a5ee2bc3b56ff664d6398a26c2514cd0
Mike Schenk <michael.schenk@extendhealth.com>
2017-12-14 11:12:16 -0700

    Removed artifacts of the previous outbox implementation.

--- src/Jasper.Marten/MartenExtension.cs
+++ src/Jasper.Marten/MartenExtension.cs
@@ -8,7 +8,6 @@ using Jasper.Configuration;
 using Jasper.Marten;
 using Jasper.Marten.Codegen;
 using Marten;
-using Marten.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 // SAMPLE: MartenExtension
@@ -22,13 +21,10 @@ namespace Jasper.Marten
         {
             registry.Settings.Require<StoreOptions>();
 
-            registry.Services.AddSingleton<SessionCommitListener>();
-
             registry.Services.AddSingleton<IDocumentStore>(x =>
             {
                 var storeOptions = x.GetService<StoreOptions>();
                 var documentStore = new DocumentStore(storeOptions);
-                storeOptions.Listeners.Add(x.GetService<SessionCommitListener>());
                 return documentStore;
             });
 
@@ -40,61 +36,6 @@ namespace Jasper.Marten
         }
     }
 
-    /// <summary>
-    /// This class is used by the MartenOutboxBus class to react to the commit of sessions it attaches to.
-    /// </summary>
-    public class SessionCommitListener : DocumentSessionListenerBase
-    {
-        private readonly ConcurrentDictionary<IDocumentSession, List<Action>> _commitCallbacks
-            = new ConcurrentDictionary<IDocumentSession, List<Action>>();
-
-        public Action RegisterCallbackAfterCommit(IDocumentSession session, Action callback)
-        {
-            var list = _commitCallbacks.GetOrAdd(session, s => new List<Action>());
-            // Note: we have one list per IDocumentSession and expect any given IDocumentSession instance
-            // to be used by only one thread at a time, so we're not synchronizing access to the list, just
-            // the dictionary that's shared by all sessions.
-            list.Add(callback);
-
-            // this is the unregistration function
-            void UnregisterCallback()
-            {
-                // Again, we're not concerned about thread-safety: the unregistration shouldn't be
-                // happening simultaneously with any RegisterCallback, or even AfterCommit of the same session.
-                list.Remove(callback);
-                if (list.Count == 0)
-                {
-                    _commitCallbacks.TryRemove(session, out var theOldList);
-                }
-            }
-
-            return UnregisterCallback;
-        }
-
-        public override void AfterCommit(IDocumentSession session, IChangeSet commit)
-        {
-            if (_commitCallbacks.TryGetValue(session, out var callbacks))
-            {
-                foreach (var callback in callbacks)
-                {
-                    callback.Invoke();
-                }
-            }
-        }
-
-        public override Task AfterCommitAsync(IDocumentSession session, IChangeSet commit, CancellationToken token)
-        {
-            if (_commitCallbacks.TryGetValue(session, out var callbacks))
-            {
-                foreach (var callback in callbacks)
-                {
-                    callback.Invoke();
-                }
-            }
-            return Task.CompletedTask;
-        }
-
-    }
 
 }
 // ENDSAMPLE
