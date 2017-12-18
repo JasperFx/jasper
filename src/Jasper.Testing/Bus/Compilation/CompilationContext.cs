619fb75e15808cbcfba097e41355fb156485e25b
Mike Schenk <michael.schenk@extendhealth.com>
2017-12-18 11:28:46 -0700

    Fixing build.

--- src/Jasper.Testing/Bus/Compilation/CompilationContext.cs
+++ src/Jasper.Testing/Bus/Compilation/CompilationContext.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Jasper.Bus;
 using Jasper.Bus.Model;
 using Jasper.Bus.Runtime;
 using Jasper.Bus.Runtime.Invocation;
