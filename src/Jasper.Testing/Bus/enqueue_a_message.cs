955ee9aba4a60f31a7dbddf5d17353001ad05fb7
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-01-31 07:59:22 -0600

    Fixed another failing test

--- src/Jasper.Testing/Bus/enqueue_a_message.cs
+++ src/Jasper.Testing/Bus/enqueue_a_message.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Baseline;
+using Baseline.Dates;
 using Jasper.Bus;
 using Jasper.Testing.Bus.Compilation;
 using Jasper.Testing.Bus.Runtime;
@@ -56,7 +57,6 @@ namespace Jasper.Testing.Bus
          public async Task enqueue_locally_lightweight()
          {
              var registry = new JasperRegistry();
-             registry.Handlers.DisableConventionalDiscovery();
              registry.Http.Actions.DisableConventionalDiscovery();
 
 
@@ -65,6 +65,7 @@ namespace Jasper.Testing.Bus
              registry.Services.AddTransient<IFakeService, FakeService>();
              registry.Services.AddTransient<IWidget, Widget>();
              registry.Services.AddTransient<IMyService, MyService>();
+             registry.Services.AddTransient<IPongWriter, PongWriter>();
 
              var tracker = new MessageTracker();
              registry.Services.AddSingleton(tracker);
@@ -79,7 +80,8 @@ namespace Jasper.Testing.Bus
 
                  await runtime.Get<IServiceBus>().EnqueueLightweight(message);
 
-                 var received = await waiter;
+                 waiter.Wait(5.Seconds());
+                 var received = waiter.Result;
 
                  received.Message.As<Message1>().Id.ShouldBe(message.Id);
              }
