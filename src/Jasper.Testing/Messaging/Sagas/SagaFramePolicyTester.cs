8ba4c1f58425bfd88c6d236cdf6fba323eb2ea28
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-04-03 07:50:17 -0500

    can choose a saga id for a messsage type

---
+++ src/Jasper.Testing/Messaging/Sagas/SagaFramePolicyTester.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Jasper.Messaging.Sagas;
+using Shouldly;
+using Xunit;
+
+namespace Jasper.Testing.Messaging.Sagas
+{
+    public class SagaFramePolicyTester
+    {
+        [Theory]
+        [InlineData(typeof(WithIdProp), "SagaId")]
+        [InlineData(typeof(WithAttribute), "Name")]
+        public void choose_saga_id_prop(Type messageType, string propertyName)
+        {
+            SagaFramePolicy.ChooseSagaIdProperty(messageType)
+                .Name.ShouldBe(propertyName);
+        }
+    }
+
+    public class WithIdProp
+    {
+        public string SagaId { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class WithAttribute
+    {
+        public string SagaId { get; set; }
+
+        [SagaId]
+        public string Name { get; set; }
+    }
+}
---
+++ src/Jasper/Messaging/Sagas/SagaFramePolicy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Baseline.Reflection;
+using Jasper.Messaging.Configuration;
+using Jasper.Messaging.Model;
+
+namespace Jasper.Messaging.Sagas
+{
+    public class SagaFramePolicy : IHandlerPolicy
+    {
+        public const string SagaIdPropertyName = "SagaId";
+
+        public void Apply(HandlerGraph graph)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static PropertyInfo ChooseSagaIdProperty(Type messageType)
+        {
+            return messageType.GetProperties().FirstOrDefault(x => x.HasAttribute<SagaIdAttribute>())
+                   ?? messageType.GetProperties().FirstOrDefault(x => x.Name == SagaIdPropertyName);
+        }
+    }
+
+    /// <summary>
+    /// Marks a public property on a message type as the saga state identity
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SagaIdAttribute : Attribute
+    {
+
+    }
+}
