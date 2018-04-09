42957b6f4dc669d1bf57fe546f37d0f31be41169
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-04-09 09:15:58 -0500

    new custom exceptions for saga validation for laters

---
+++ src/Jasper/Messaging/Sagas/UnknownSagaStateException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Jasper.Messaging.Runtime;
+using Lamar.Codegen;
+
+namespace Jasper.Messaging.Sagas
+{
+    public class IndeterminateSagaStateIdException : Exception
+    {
+        public IndeterminateSagaStateIdException(Envelope envelope) : base($"Could not determine a valid saga state id for Envelope {envelope}")
+        {
+        }
+    }
+
+    public class UnknownSagaStateException : Exception
+    {
+        public UnknownSagaStateException(Type sagaStateType, object stateId) : base($"Could not find an expected state document of type {sagaStateType.FullNameInCode()} for id '{stateId}'")
+        {
+        }
+    }
+
+
+}
