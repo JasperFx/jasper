81ad0a9c904656f7385ce2b9af72b1619f2827e1
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-04-10 20:22:13 -0500

    validate command compiles all the handlers too. Closes GH-351

--- src/Jasper.CommandLine/ValidateCommand.cs
+++ src/Jasper.CommandLine/ValidateCommand.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Jasper.Messaging.Model;
+using Lamar.Codegen;
+using Lamar.Compilation;
 using Oakton;
 
 namespace Jasper.CommandLine
@@ -11,7 +14,17 @@ namespace Jasper.CommandLine
             Console.WriteLine("Bootstrapping the system and running all checks...");
             using (var runtime = input.BuildRuntime())
             {
-                // nothing really to do
+                Console.WriteLine("Generating code for all the message handlers, this might take a bit...");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                var rules = runtime.Get<GenerationRules>();
+                var generatedAssembly = new GeneratedAssembly(rules);
+                var handlers = runtime.Get<HandlerGraph>();
+                foreach (var handler in handlers.Chains)
+                {
+                    handler.AssembleType(generatedAssembly);
+                }
             }
 
             ConsoleWriter.Write(ConsoleColor.Green, "All systems good!");
