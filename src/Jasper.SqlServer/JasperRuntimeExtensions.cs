7438bba9c3f43ac487f8c57253aec07c90a7206a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-04-24 09:06:00 -0500

    Little helper to rebuild messaging persistence schema objects

---
+++ src/Jasper.SqlServer/JasperRuntimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Jasper.SqlServer.Schema;
+
+namespace Jasper.SqlServer
+{
+    public static class JasperRuntimeExtensions
+    {
+        /// <summary>
+        /// Drops and recreates the Sql Server backed persistence database objects
+        /// </summary>
+        /// <param name="runtime"></param>
+        public static void RebuildMessageStorage(this JasperRuntime runtime)
+        {
+            var settings = runtime.Get<SqlServerSettings>();
+            var builder = new SchemaLoader(settings.ConnectionString, settings.SchemaName);
+            builder.RecreateAll();
+        }
+    }
+}
