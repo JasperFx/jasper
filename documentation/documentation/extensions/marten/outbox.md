888c238e613474744c8326f9522ecc314afdb39b
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-05-04 13:39:08 -0500

    docs on outbox usage with Marten

--- documentation/documentation/extensions/marten/outbox.md
+++ documentation/documentation/extensions/marten/outbox.md
@@ -1,3 +1,17 @@
 <!--title:Outbox Pattern with Marten-->
 
-TODO -- actual content.
+See [Jasper’s “Outbox” Pattern Support](https://jeremydmiller.com/2018/04/16/jaspers-outbox-pattern-support/) for more context around why you would care about the "outbox" pattern.
+
+Jasper supports the ["outbox" pattern](https://jimmybogard.com/refactoring-towards-resilience-evaluating-coupling/) with [Marteh](https://jasperfx.github.io/marten) sessions. You can explicitly opt into this usage with code like this:
+
+<[sample:ExplicitOutboxUsage]>
+
+Do note that with the Marten-backed outbox usage, Jasper is able to listen for `IDocumentSession.SaveChanges()` or `IDocumentSession.SaveChangesAsync()` and flush out the persisted, outgoing messages into the actual sending queues. There is
+no need to do anything else for the messages to be sent out to other services.
+
+If you use the `[MartenTransaction]` middleware in a message handler, the middleware will take care of some of the repetitive mechanics for you. In the code below, the `IMessageContext` behind the scenes of the message being currently handled is enrolled in the current transaction represented by the current Marten `IDocumentSession` before the action runs, and the outgoing messages
+are flushed into the outgoing sending queue after the action runs.
+
+<[sample:MartenCreateItemHandler]>
+
+See also <[linkto:documentation/extensions/sqlserver/outbox]>
--- src/ShowHandler/CreateItemHandler.cs
+++ src/ShowHandler/CreateItemHandler.cs
@@ -1,9 +1,11 @@
 ﻿using System.Threading.Tasks;
 using Jasper.Marten;
+using Jasper.Messaging;
 using Marten;
 
 namespace ShowHandler
 {
+    /*
     public class CreateItemHandler
     {
         private readonly IDocumentStore _store;
@@ -25,6 +27,7 @@ namespace ShowHandler
             }
         }
     }
+    */
 
     /*
     public class CreateItemHandler
@@ -46,16 +49,55 @@ namespace ShowHandler
     */
 
 
-    /*
+    public static class ExplicitOutboxUsage
+    {
+        // SAMPLE: ExplicitOutboxUsage
+        public static async Task explicit_usage(
+            CreateItemCommand command,
+            IDocumentStore store,
+            IMessageContext context)
+        {
+            var item = new Item {Name = command.Name};
+
+            using (var session = store.LightweightSession())
+            {
+                // This tells the current Jasper message context
+                // to persist outgoing messages with this
+                // Marten session and to delay actually sending
+                // out the outgoing messages until the session
+                // has been committed
+                await context.EnlistInTransaction(session);
+
+                session.Store(item);
+
+                // Registering an outgoing message, nothing is
+                // actually "sent" yet though
+                await context.Publish(new ItemCreatedEvent {Item = item});
+
+                // Commit all the queued up document and outgoing messages
+                // to the underlying Postgresql database.
+                await session.SaveChangesAsync();
+            }
+        }
+        // ENDSAMPLE
+    }
+
+    // SAMPLE: MartenCreateItemHandler
     public class CreateItemHandler
     {
         [MartenTransaction]
         public static ItemCreatedEvent Handle(CreateItemCommand command, IDocumentSession session)
         {
+            // This Item document and the outgoing ItemCreatedEvent
+            // message being published will be persisted in
+            // the same native Postgresql transaction
             var item = new Item {Name = command.Name};
             session.Store(item);
 
+            // The outgoing message here is persisted with Marten
+            // as part of the same transaction
             return new ItemCreatedEvent{Item = item};
         }
-    }*/
+    }
+    // ENDSAMPLE
 }
