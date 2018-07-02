c3bdaa359336f15671615b89554d7bad7ec152dd
jeremydmiller <jeremydmiller@yahoo.com>
2018-07-02 06:44:16 -0500

    extra 'remembered' resolvers

--- src/Jasper/Internal/Resolvers/Jasper_Messaging_Logging_ITransportLogger_transportLogger1.cs
+++ src/Jasper/Internal/Resolvers/Jasper_Messaging_Logging_ITransportLogger_transportLogger1.cs
@@ -7,12 +7,12 @@ namespace Jasper.Internal.Resolvers
     // START: Jasper_Messaging_Logging_ITransportLogger_transportLogger1
     public class Jasper_Messaging_Logging_ITransportLogger_transportLogger1 : Lamar.IoC.Resolvers.SingletonResolver<Jasper.Messaging.Logging.ITransportLogger>
     {
-        private readonly Microsoft.Extensions.Logging.ILoggerFactory _loggerFactory523511804;
+        private readonly Microsoft.Extensions.Logging.ILoggerFactory _loggerFactory762265419;
         private readonly Lamar.IoC.Scope _topLevelScope;
 
-        public Jasper_Messaging_Logging_ITransportLogger_transportLogger1([Lamar.Named("loggerFactory2")] Microsoft.Extensions.Logging.ILoggerFactory loggerFactory523511804, Lamar.IoC.Scope topLevelScope) : base(topLevelScope)
+        public Jasper_Messaging_Logging_ITransportLogger_transportLogger1([Lamar.Named("loggerFactory2")] Microsoft.Extensions.Logging.ILoggerFactory loggerFactory762265419, Lamar.IoC.Scope topLevelScope) : base(topLevelScope)
         {
-            _loggerFactory523511804 = loggerFactory523511804;
+            _loggerFactory762265419 = loggerFactory762265419;
             _topLevelScope = topLevelScope;
         }
 
@@ -21,7 +21,7 @@ namespace Jasper.Internal.Resolvers
         public override Jasper.Messaging.Logging.ITransportLogger Build(Lamar.IoC.Scope scope)
         {
             var nulloMetrics = new Jasper.Messaging.Logging.NulloMetrics();
-            return new Jasper.Messaging.Logging.TransportLogger(_loggerFactory523511804, nulloMetrics);
+            return new Jasper.Messaging.Logging.TransportLogger(_loggerFactory762265419, nulloMetrics);
         }
 
     }
--- src/Jasper/Internal/Resolvers/Jasper_Messaging_Transports_ITransport_tcpTransport.cs
+++ src/Jasper/Internal/Resolvers/Jasper_Messaging_Transports_ITransport_tcpTransport.cs
@@ -7,12 +7,12 @@ namespace Jasper.Internal.Resolvers
     // START: Jasper_Messaging_Transports_ITransport_tcpTransport
     public class Jasper_Messaging_Transports_ITransport_tcpTransport : Lamar.IoC.Resolvers.TransientResolver<Jasper.Messaging.Transports.ITransport>
     {
-        private readonly Jasper.Messaging.Logging.ITransportLogger _transportLogger_312967181;
+        private readonly Jasper.Messaging.Logging.ITransportLogger _transportLogger421692895;
         private readonly Jasper.Messaging.Transports.Configuration.MessagingSettings _messagingSettings;
 
-        public Jasper_Messaging_Transports_ITransport_tcpTransport([Lamar.Named("transportLogger2")] Jasper.Messaging.Logging.ITransportLogger transportLogger_312967181, Jasper.Messaging.Transports.Configuration.MessagingSettings messagingSettings)
+        public Jasper_Messaging_Transports_ITransport_tcpTransport([Lamar.Named("transportLogger2")] Jasper.Messaging.Logging.ITransportLogger transportLogger421692895, Jasper.Messaging.Transports.Configuration.MessagingSettings messagingSettings)
         {
-            _transportLogger_312967181 = transportLogger_312967181;
+            _transportLogger421692895 = transportLogger421692895;
             _messagingSettings = messagingSettings;
         }
 
@@ -20,8 +20,8 @@ namespace Jasper.Internal.Resolvers
 
         public override Jasper.Messaging.Transports.ITransport Build(Lamar.IoC.Scope scope)
         {
-            var nulloDurableMessagingFactory = new Jasper.Messaging.Transports.NulloDurableMessagingFactory(_transportLogger_312967181, _messagingSettings);
-            return new Jasper.Messaging.Transports.Tcp.TcpTransport(nulloDurableMessagingFactory, _transportLogger_312967181, _messagingSettings);
+            var nulloDurableMessagingFactory = new Jasper.Messaging.Transports.NulloDurableMessagingFactory(_transportLogger421692895, _messagingSettings);
+            return new Jasper.Messaging.Transports.Tcp.TcpTransport(nulloDurableMessagingFactory, _transportLogger421692895, _messagingSettings);
         }
 
     }
