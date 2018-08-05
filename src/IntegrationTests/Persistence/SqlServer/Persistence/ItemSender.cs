3d6f393c49bff081f892c7811b4fb8a084dac615
jeremydmiller <jeremydmiller@yahoo.com>
2018-08-05 15:51:01 -0500

    removed integration tests that moved to Storyteller

--- src/IntegrationTests/Persistence/SqlServer/Persistence/ItemSender.cs
+++ src/IntegrationTests/Persistence/SqlServer/Persistence/ItemSender.cs
@@ -8,23 +8,7 @@ using Servers.Docker;
 
 namespace IntegrationTests.Persistence.SqlServer.Persistence
 {
-    public class ItemSender : JasperRegistry
-    {
-        public ItemSender()
-        {
-            Settings.PersistMessagesWithSqlServer(SqlServerContainer.ConnectionString, "sender");
 
-
-
-            Publish.Message<ItemCreated>().To("tcp://localhost:2345/durable");
-            Publish.Message<Question>().To("tcp://localhost:2345/durable");
-
-            Transports.LightweightListenerAt(2567);
-
-
-
-        }
-    }
 
     public class SendItemEndpoint
     {
