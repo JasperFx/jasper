f9b0898a6f304df802781ed95a2e386a22e0eae9
jeremydmiller <jeremydmiller@yahoo.com>
2019-01-10 06:52:56 -0500

    Little refactoring for how route segments are passed down in the frames

--- src/Jasper/Http/Routing/Codegen/ParsedRouteArgumentFrame.cs
+++ src/Jasper/Http/Routing/Codegen/ParsedRouteArgumentFrame.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using Jasper.Http.Model;
 using LamarCompiler;
+using LamarCompiler.Model;
 using Microsoft.AspNetCore.Http;
 
 namespace Jasper.Http.Routing.Codegen
 {
     public class ParsedRouteArgumentFrame : RouteArgumentFrame
     {
+        private Variable _segments;
+
         public ParsedRouteArgumentFrame(Type type, string name, int position) : base(name, position, type)
         {
         }
@@ -15,7 +19,7 @@ namespace Jasper.Http.Routing.Codegen
         {
             var alias = RoutingFrames.TypeOutputs[Variable.VariableType];
             writer.WriteLine($"{alias} {Variable.Usage};");
-            writer.Write($"BLOCK:if (!{alias}.TryParse(segments[{Position}], out {Variable.Usage}))");
+            writer.Write($"BLOCK:if (!{alias}.TryParse({_segments.Usage}[{Position}], out {Variable.Usage}))");
             writer.WriteLine(
                 $"{RouteGraph.Context}.{nameof(HttpContext.Response)}.{nameof(HttpResponse.StatusCode)} = 400;");
             writer.WriteLine(method.ToExitStatement());
@@ -23,6 +27,12 @@ namespace Jasper.Http.Routing.Codegen
 
             writer.BlankLine();
             Next?.GenerateCode(method, writer);
+        }
+
+        public override IEnumerable<Variable> FindVariables(IMethodVariables chain)
+        {
+            _segments = chain.FindVariableByName(typeof(string[]), RoutingFrames.Segments);
+            yield return _segments;
         }
     }
 }
