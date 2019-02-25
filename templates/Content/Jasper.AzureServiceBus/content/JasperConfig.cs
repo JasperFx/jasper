e22a6cef80b7889470861a6a05d16ee83c559837
jeremydmiller <jeremydmiller@yahoo.com>
2019-02-25 15:39:55 -0600

    first cut at an Azure Service Bus dotnet new template

---
+++ templates/Content/Jasper.AzureServiceBus/content/JasperConfig.cs
@@ -0,0 +1,22 @@
+using System;
+using Jasper;
+using Jasper.CommandLine;
+using Microsoft.Extensions.Configuration;
+
+namespace JasperService
+{
+    internal class JasperConfig : JasperRegistry
+    {
+        public JasperConfig()
+        {
+            // Add any necessary jasper options
+
+            // Message publishing rules
+            Publish.AllMessagesToUriValueInConfig("outbound");
+
+            // Register listeners
+            Transports.ListenForMessagesFromUriValueInConfig("inbound");
+        }
+    }
+
+}
