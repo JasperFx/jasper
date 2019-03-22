3d5494ab01519e2c102ff9350a663ffaac4820dc
jeremydmiller <jeremydmiller@yahoo.com>
2019-03-22 14:16:57 -0500

    Pulled the storyteller support tests to its own lib

--- src/Jasper.TestSupport.Tests/JasperSystemTester.cs
+++ src/Jasper.TestSupport.Tests/JasperSystemTester.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Jasper;
 using Jasper.TestSupport.Storyteller;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -11,7 +10,7 @@ using Shouldly;
 using StoryTeller;
 using Xunit;
 
-namespace IntegrationTests.Storyteller
+namespace Jasper.TestSupport.Tests
 {
     [Collection("integration")]
     public class JasperSystemTester
---
+++ src/Jasper.TestSupport.Tests/Parallelization.cs
@@ -0,0 +1,3 @@
+﻿
+
+//[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
