2e0b07c8a13120a1e7c3d556d533ffbf016ce5e3
jeremydmiller <jeremydmiller@yahoo.com>
2019-04-29 08:20:47 -0500

    marking as 0.9.9.1

--- rakefile.rb
+++ rakefile.rb
@@ -2,11 +2,11 @@ require 'json'
 
 APIKEY = ENV['api_key'].nil? ? '' : ENV['api_key']
 
-BUILD_VERSION =  ENV['version'].nil? ? '0.9.9' : ENV['version']
+BUILD_VERSION =  ENV['version'].nil? ? '0.9.9.1' : ENV['version']
 puts "Build version is #{BUILD_VERSION}"
 
 
-TEMPLATE_VERSION = '0.9.9'
+TEMPLATE_VERSION = '0.9.9.1'
 COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
 RESULTS_DIR = "artifacts"
 
