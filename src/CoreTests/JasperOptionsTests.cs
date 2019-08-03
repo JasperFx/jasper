66e92ec895e850ec14cf6bef9f5d80686c1f1f46
jeremydmiller <jeremydmiller@yahoo.com>
2019-08-03 14:57:53 -0500

    Extra test on JasperOptions for unique id creation

--- src/CoreTests/JasperOptionsTests.cs
+++ src/CoreTests/JasperOptionsTests.cs
@@ -35,5 +35,36 @@ namespace CoreTests
             theSettings.EnableTransport("tcp");
             theSettings.StateFor("tcp").ShouldBe(TransportState.Enabled);
         }
+
+        [Fact]
+        public void unique_node_id_is_really_unique()
+        {
+            var options1 = new JasperOptions();
+            var options2 = new JasperOptions();
+            var options3 = new JasperOptions();
+            var options4 = new JasperOptions();
+            var options5 = new JasperOptions();
+            var options6 = new JasperOptions();
+
+            options1.UniqueNodeId.ShouldNotBe(options2.UniqueNodeId);
+            options1.UniqueNodeId.ShouldNotBe(options3.UniqueNodeId);
+            options1.UniqueNodeId.ShouldNotBe(options4.UniqueNodeId);
+            options1.UniqueNodeId.ShouldNotBe(options5.UniqueNodeId);
+            options1.UniqueNodeId.ShouldNotBe(options6.UniqueNodeId);
+
+            options2.UniqueNodeId.ShouldNotBe(options3.UniqueNodeId);
+            options2.UniqueNodeId.ShouldNotBe(options4.UniqueNodeId);
+            options2.UniqueNodeId.ShouldNotBe(options5.UniqueNodeId);
+            options2.UniqueNodeId.ShouldNotBe(options6.UniqueNodeId);
+
+            options3.UniqueNodeId.ShouldNotBe(options4.UniqueNodeId);
+            options3.UniqueNodeId.ShouldNotBe(options5.UniqueNodeId);
+            options3.UniqueNodeId.ShouldNotBe(options6.UniqueNodeId);
+
+            options4.UniqueNodeId.ShouldNotBe(options5.UniqueNodeId);
+            options4.UniqueNodeId.ShouldNotBe(options6.UniqueNodeId);
+
+            options5.UniqueNodeId.ShouldNotBe(options6.UniqueNodeId);
+        }
     }
 }
