b5b91f095ad9d9fc99c18c64742d5ef9f9291c0e
jeremydmiller <jeremydmiller@yahoo.com>
2019-12-13 09:28:06 -0600

    Making sure that automatic recovery is enabled by default

---
+++ src/Jasper.RabbitMQ.Tests/RabbitMqTransportTester.cs
@@ -0,0 +1,16 @@
+using Jasper.RabbitMQ.Internal;
+using Shouldly;
+using Xunit;
+
+namespace Jasper.RabbitMQ.Tests
+{
+    public class RabbitMqTransportTester
+    {
+        [Fact]
+        public void automatic_recovery_is_try_by_default()
+        {
+            var transport = new RabbitMqTransport();
+            transport.ConnectionFactory.AutomaticRecoveryEnabled.ShouldBeTrue();
+        }
+    }
+}
