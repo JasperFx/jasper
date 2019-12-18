606cbc0d60b4b24166c2dd351590c093fb91ea6d
jeremydmiller <jeremydmiller@yahoo.com>
2019-12-18 09:58:48 -0600

    Verifying that users can apply custom protocols

---
+++ src/Jasper.RabbitMQ.Tests/configuring_a_custom_protocol_per_endpoint.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using Jasper.Configuration;
+using Jasper.Messaging;
+using Jasper.RabbitMQ.Internal;
+using LamarCodeGeneration.Util;
+using Microsoft.Extensions.Hosting;
+using Shouldly;
+using Xunit;
+
+namespace Jasper.RabbitMQ.Tests
+{
+    public class configuring_a_custom_protocol_per_endpoint
+    {
+        [Fact]
+        public void can_register_a_custom_protocol_per_endpoint()
+        {
+            using (var host = JasperHost.For<CustomProtocolApp>())
+            {
+                var endpoint = host.Get<IMessagingRoot>().Options.Endpoints.As<TransportCollection>()
+                    .AllEndpoints().OfType<RabbitMqEndpoint>().Single();
+
+                endpoint.Protocol.ShouldBeOfType<CustomProtocol>();
+            }
+        }
+
+        public class CustomProtocolApp : JasperOptions
+        {
+            public CustomProtocolApp()
+            {
+                Endpoints.ConfigureRabbitMq(x =>
+                {
+                    x.ConnectionFactory.HostName = "localhost";
+                    x.AutoProvision = true;
+                });
+
+                Endpoints.ListenToRabbitQueue("queue1").Protocol<CustomProtocol>();
+            }
+        }
+
+        public class CustomProtocol : DefaultRabbitMqProtocol
+        {
+
+        }
+    }
+}
