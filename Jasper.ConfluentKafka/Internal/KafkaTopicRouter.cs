821f806b132f0edde94609bafc35c626122c75c0
Jarrod Johnson <jjohnson@new-innov.com>
2020-04-17 10:21:04 -0400

    Add generics for Kafka producer

---
+++ Jasper.ConfluentKafka/Internal/KafkaTopicRouter.cs
@@ -0,0 +1,36 @@
+using System;
+using Baseline;
+using Jasper.Configuration;
+using Jasper.Runtime.Routing;
+
+namespace Jasper.ConfluentKafka.Internal
+{
+    public class KafkaTopicRouter : TopicRouter<KafkaSubscriberConfiguration>
+    {
+        public KafkaTopicRouter()
+        {
+        }
+
+        public override Uri BuildUriForTopic(string topicName)
+        {
+            var endpoint = new KafkaEndpoint
+            {
+                IsDurable = true,
+                TopicName = topicName
+            };
+
+            return endpoint.Uri;
+        }
+
+        public override KafkaSubscriberConfiguration FindConfigurationForTopic(string topicName,
+            IEndpoints endpoints)
+        {
+            var uri = BuildUriForTopic(topicName);
+            var endpoint = endpoints.As<TransportCollection>().GetOrCreateEndpoint(uri);
+
+            return new KafkaSubscriberConfiguration((KafkaEndpoint) endpoint);
+        }
+
+    }
+
+}
