0aa0765734477d50bf05b7d3a2a0fc964f2c1e03
jeremydmiller <jeremydmiller@yahoo.com>
2020-04-21 20:58:56 -0500

    demonstrator project for EF Core + Sql Server tx usage

---
+++ src/EFPlusSqlServerConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Jasper;
+using Microsoft.Extensions.Hosting;
+
+namespace EFPlusSqlServerConsole
+{
+    class Program
+    {
+        public static Task<int> Main(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+
+                .UseJasper<JasperConfig>()
+                .RunJasper(args);
+        }
+    }
+}
