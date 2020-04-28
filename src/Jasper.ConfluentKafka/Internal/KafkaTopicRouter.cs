80de3a5a719dccee9897b1df26665168f9ca9b67
Jarrod Johnson <jjohnson@new-innov.com>
2020-04-28 12:08:35 -0400

    Remove commented code and unused usings

--- src/Jasper.ConfluentKafka/Internal/KafkaTopicRouter.cs
+++ src/Jasper.ConfluentKafka/Internal/KafkaTopicRouter.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using Baseline;
-using Confluent.Kafka;
 using Jasper.Configuration;
 using Jasper.Runtime.Routing;
 
@@ -9,11 +7,6 @@ namespace Jasper.ConfluentKafka.Internal
 {
     public class KafkaTopicRouter : TopicRouter<KafkaSubscriberConfiguration>
     {
-        //Dictionary<Uri, ProducerConfig> producerConfigs;
-        //public KafkaTopicRouter(Dictionary<Uri, ProducerConfig> producerConfigs)
-        //{
-        //}
-
         public override Uri BuildUriForTopic(string topicName)
         {
             var endpoint = new KafkaEndpoint
@@ -33,7 +26,5 @@ namespace Jasper.ConfluentKafka.Internal
 
             return new KafkaSubscriberConfiguration((KafkaEndpoint) endpoint);
         }
-
     }
-
 }
