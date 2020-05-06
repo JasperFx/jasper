b9225eb8b23186083c5241d3b03356840d884323
Jarrod Johnson <jjohnson@new-innov.com>
2020-05-06 15:18:46 -0400

    Add IRequireSenderCallback and implementation

--- src/Jasper/Transports/Sending/ISender.cs
+++ src/Jasper/Transports/Sending/ISender.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Jasper.Transports.Sending
 {
+    public interface ISenderRequiresCallback : IDisposable
+    {
+        void RegisterCallback(ISenderCallback senderCallback);
+    }
+
     public interface ISender : IDisposable
     {
+        bool SupportsNativeScheduledSend { get; }
         Uri Destination { get; }
-        
+        Task<bool> Ping(CancellationToken cancellationToken);
         Task Send(Envelope envelope);
     }
 }
