b137c96cb92cc7f3cb1a7aef6a5e08d6a7325459
Jarrod Johnson <jjohnson@new-innov.com>
2020-05-29 17:47:32 -0400

    Pass pulsar MessageId for mappings

--- src/Jasper.Pulsar/Internal/PulsarMessage.cs
+++ src/Jasper.Pulsar/Internal/PulsarMessage.cs
@@ -6,9 +6,24 @@ namespace Jasper.Pulsar.Internal
 {
     internal class PulsarMessage
     {
+        public MessageId MessageId { get; set; }
         public MessageMetadata Metadata { get; } = new MessageMetadata();
         public ReadOnlySequence<byte> Data { get; }
         public IReadOnlyDictionary<string, string> Properties { get; } = new Dictionary<string, string>();
+
+        public PulsarMessage(Message message)
+        {
+            MessageId = message.MessageId;
+            Metadata.Key = message.Key;
+            Metadata.SequenceId = message.SequenceId;
+            Metadata.EventTime = message.EventTime;
+            Metadata.EventTimeAsDateTimeOffset = message.EventTimeAsDateTimeOffset;
+            Metadata.KeyBytes = message.KeyBytes;
+            Metadata.OrderingKey = message.OrderingKey;
+
+            Properties = message.Properties;
+            Data = message.Data;
+        }
 
         public PulsarMessage(ReadOnlySequence<byte> data)
         {
