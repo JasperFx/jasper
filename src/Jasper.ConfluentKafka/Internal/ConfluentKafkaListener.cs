63bd4151b8ac18dc6538e26fee95ecf50b45ae0c
Jarrod Johnson <jjohnson@new-innov.com>
2020-06-04 16:48:00 -0400

    Implement inline receive in Kafka

--- src/Jasper.ConfluentKafka/Internal/ConfluentKafkaListener.cs
+++ src/Jasper.ConfluentKafka/Internal/ConfluentKafkaListener.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Jasper.ConfluentKafka;
+using Jasper.ConfluentKafka.Internal;
 using Jasper.Logging;
 using Jasper.Runtime;
 using Jasper.Transports;
@@ -17,6 +18,7 @@ namespace Jasper.Kafka.Internal
         private readonly KafkaEndpoint _endpoint;
         private readonly ITransportLogger _logger;
         private IListeningWorkerQueue _callback;
+        private IHandlerPipeline _pipeline;
         private readonly ITransportProtocol<Message<byte[], byte[]>> _protocol;
         private Task _consumerTask;
 
@@ -51,56 +53,99 @@ namespace Jasper.Kafka.Internal
 
         public void StartHandlingInline(IHandlerPipeline pipeline)
         {
-            throw new NotImplementedException();
+            _pipeline = pipeline;
+
+            _consumer.Subscribe(new[] { _endpoint.TopicName });
+
+            _consumerTask = ConsumeInlineAsync();
+
+            _logger.ListeningStatusChange(ListeningStatus.Accepting);
         }
 
         private async Task ConsumeAsync()
         {
             while (!_cancellation.IsCancellationRequested)
             {
-                ConsumeResult<byte[], byte[]> message;
-                try
-                {
-                    message = await Task.Run(() => _consumer.Consume(), _cancellation);
-                }
-                catch (Confluent.Kafka.ConsumeException cex)
-                {
-                    if (cex.Error.Code == ErrorCode.PolicyViolation)
-                    {
-                        throw;
-                    }
+                ConsumeResult<byte[], byte[]> result = await NextMessageAsync();
 
+                if (result == null)
                     continue;
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogException(ex, message: $"Error consuming message from Kafka topic {_endpoint.TopicName}");
-                    continue;
-                }
 
-                Envelope envelope;
+                Envelope envelope = DeserializeResult(result);
 
-                try
-                {
-                    envelope = _protocol.ReadEnvelope(message.Message);
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogException(ex, message: $"Error trying to map an incoming Kafka {_endpoint.TopicName} Topic message to an Envelope. See the Dead Letter Queue");
+                if (envelope == null)
                     continue;
-                }
 
                 try
                 {
                     await _callback.Received(Address, envelope);
-
-                    _consumer.Commit(message);
                 }
                 catch (Exception e)
                 {
-                    // TODO -- Got to either discard this or defer it back to the queue
                     _logger.LogException(e, envelope.Id, "Error trying to receive a message from " + Address);
                 }
+            }
+        }
+
+        private async Task ConsumeInlineAsync()
+        {
+            while (!_cancellation.IsCancellationRequested)
+            {
+                ConsumeResult<byte[], byte[]> result = await NextMessageAsync();
+
+                if(result == null)
+                    continue;
+
+                Envelope envelope = DeserializeResult(result);
+
+                if(envelope == null)
+                    continue;
+
+                try
+                {
+                    await _pipeline.Invoke(envelope, new KafkaChannelCallback(result, _consumer));
+                }
+                catch (Exception e)
+                {
+                    _logger.LogException(e, envelope.Id, "Error trying to process message inline from " + Address);
+                }
+            }
+        }
+
+
+        Envelope DeserializeResult(ConsumeResult<byte[], byte[]> result)
+        {
+            try
+            {
+                return _protocol.ReadEnvelope(result.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogException(ex, message: $"Error trying to map an incoming Kafka {_endpoint.TopicName} Topic message to an Envelope. See the Dead Letter Queue");
+                return null;
+            }
+        }
+
+        async Task<ConsumeResult<byte[], byte[]>> NextMessageAsync()
+        {
+            try
+            {
+                ConsumeResult<byte[], byte[]> next = await Task.Run(() => _consumer.Consume(), _cancellation);
+                return next;
+            }
+            catch (Confluent.Kafka.ConsumeException cex)
+            {
+                if (cex.Error.Code == ErrorCode.PolicyViolation)
+                {
+                    throw;
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogException(ex, message: $"Error consuming message from Kafka topic {_endpoint.TopicName}");
+                return null;
             }
         }
     }
