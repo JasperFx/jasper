e618af67e29b60610bea07d78a4e93af2966a53f
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2021-10-18 14:06:32 -0500

    collapsed DurabilityAgentStorage into DatabaseBackedEnvelopePersistence

--- src/Jasper.Persistence.Database/DatabaseBackedEnvelopePersistence.cs
+++ src/Jasper.Persistence.Database/DatabaseBackedEnvelopePersistence.cs
@@ -11,15 +11,19 @@ using Weasel.Core;
 
 namespace Jasper.Persistence.Database
 {
-    public abstract class DatabaseBackedEnvelopePersistence : DurabilityAgentStorage, IEnvelopePersistence
+    public abstract class DatabaseBackedEnvelopePersistence : IEnvelopePersistence, IDisposable
     {
         protected readonly CancellationToken _cancellation;
         private readonly string _incrementIncomingAttempts;
         private readonly string _insertOutgoingSql;
         private readonly string _storeIncoming;
+        private readonly DurableStorageSession _session;
+        private readonly string _findReadyToExecuteJobs;
+        private readonly string _fetchOwnersSql;
+        private readonly string _reassignDormantNodeSql;
 
         protected DatabaseBackedEnvelopePersistence(DatabaseSettings databaseSettings, AdvancedSettings settings,
-            IEnvelopeStorageAdmin admin) : base(databaseSettings, settings)
+            IEnvelopeStorageAdmin admin)
         {
             DatabaseSettings = databaseSettings;
             Admin = admin;
@@ -37,6 +41,40 @@ values
 ";
             _insertOutgoingSql =
                 $"insert into {DatabaseSettings.SchemaName}.{DatabaseConstants.OutgoingTable} (id, owner_id, destination, deliver_by, body) values (@id, @owner, @destination, @deliverBy, @body)";
+
+            var transaction = new DurableStorageSession(databaseSettings, settings.Cancellation);
+
+            _session = transaction;
+            Session = transaction;
+
+            // ReSharper disable once VirtualMemberCallInConstructor
+            Incoming = buildDurableIncoming(transaction, databaseSettings, settings);
+
+            // ReSharper disable once VirtualMemberCallInConstructor
+            Outgoing = buildDurableOutgoing(transaction, databaseSettings, settings);
+
+            _findReadyToExecuteJobs =
+                $"select body, attempts from {databaseSettings.SchemaName}.{DatabaseConstants.IncomingTable} where status = '{EnvelopeStatus.Scheduled}' and execution_time <= @time";
+
+            _cancellation = settings.Cancellation;
+
+            _fetchOwnersSql = $@"
+select distinct owner_id from {databaseSettings.SchemaName}.{DatabaseConstants.IncomingTable} where owner_id != 0 and owner_id != @owner
+union
+select distinct owner_id from {databaseSettings.SchemaName}.{DatabaseConstants.OutgoingTable} where owner_id != 0 and owner_id != @owner";
+
+            _reassignDormantNodeSql = $@"
+update {databaseSettings.SchemaName}.{DatabaseConstants.IncomingTable}
+  set owner_id = 0
+where
+  owner_id = @owner;
+
+update {databaseSettings.SchemaName}.{DatabaseConstants.OutgoingTable}
+  set owner_id = 0
+where
+  owner_id = @owner;
+";
+
         }
 
         public AdvancedSettings Settings { get; }
@@ -44,6 +82,11 @@ values
         public DatabaseSettings DatabaseSettings { get; }
 
         public IEnvelopeStorageAdmin Admin { get; }
+
+        public IDurableStorageSession Session { get; }
+        public IDurableIncoming Incoming { get; }
+        public IDurableOutgoing Outgoing { get; }
+
 
         public Task DeleteOutgoing(Envelope envelope)
         {
@@ -245,6 +288,51 @@ values
                         $"select body, '{EnvelopeStatus.Outgoing}', owner_id, NULL from {DatabaseSettings.SchemaName}.{DatabaseConstants.OutgoingTable}")
                     .LoadEnvelopes();
             }
+        }
+
+        public Task<Envelope[]> LoadScheduledToExecute(DateTimeOffset utcNow)
+        {
+            return _session
+                .CreateCommand(_findReadyToExecuteJobs)
+                .With("time", utcNow)
+                .ExecuteToEnvelopesWithAttempts(_cancellation, _session.Transaction);
+        }
+
+        public Task ReassignDormantNodeToAnyNode(int nodeId)
+        {
+            return _session.CreateCommand(_reassignDormantNodeSql)
+                .With("owner", nodeId)
+                .ExecuteNonQueryAsync(_cancellation);
+        }
+
+        public async Task<int[]> FindUniqueOwners(int currentNodeId)
+        {
+            var list = new List<int>();
+            using (var reader = await _session.CreateCommand(_fetchOwnersSql)
+                .With("owner", currentNodeId)
+                .ExecuteReaderAsync(_cancellation))
+            {
+                while (await reader.ReadAsync(_cancellation))
+                {
+                    var id = await reader.GetFieldValueAsync<int>(0, _cancellation);
+                    list.Add(id);
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        protected abstract IDurableOutgoing buildDurableOutgoing(DurableStorageSession durableStorageSession,
+            DatabaseSettings databaseSettings, AdvancedSettings settings);
+
+        protected abstract IDurableIncoming buildDurableIncoming(DurableStorageSession durableStorageSession,
+            DatabaseSettings databaseSettings, AdvancedSettings settings);
+
+
+
+        public void Dispose()
+        {
+            Session?.Dispose();
         }
     }
 }
