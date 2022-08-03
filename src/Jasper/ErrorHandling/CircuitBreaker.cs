d7a2f8ced2148b07db0fdc6c7f7a24299707ba89
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-08-03 10:39:18 -0500

    Basic CircuitBreaker implementation, but not integrated yet

---
+++ src/Jasper/ErrorHandling/CircuitBreaker.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Baseline.Dates;
+using Jasper.Transports;
+using Jasper.Transports.Util;
+
+namespace Jasper.ErrorHandling;
+
+internal class CircuitBreaker : IDisposable
+{
+    private readonly CircuitBreakerOptions _options;
+    private readonly IExceptionMatch _match;
+    private readonly IListeningAgent _listeningAgent;
+    private readonly CancellationTokenSource _cancellation = new CancellationTokenSource();
+    private readonly ActionBlock<object[]> _processingBlock;
+    private readonly BatchingBlock<object> _batching;
+    private readonly List<Generation> _generations = new();
+    private readonly double _ratio;
+
+    public CircuitBreaker(CircuitBreakerOptions options, IExceptionMatch match, IListeningAgent listeningAgent)
+    {
+        _options = options;
+        _match = match;
+        _listeningAgent = listeningAgent;
+
+        _processingBlock = new ActionBlock<object[]>(processExceptionsAsync);
+        _batching = new BatchingBlock<object>(options.SamplingPeriod, _processingBlock);
+
+        GenerationPeriod = ((int)Math.Floor(_options.TrackingPeriod.TotalSeconds / 4)).Seconds();
+
+        _ratio = _options.FailurePercentageThreshold / 100.0;
+    }
+
+    public TimeSpan GenerationPeriod { get; set; }
+
+
+    public bool ShouldStopProcessing()
+    {
+        var failures = _generations.Sum(x => x.Failures);
+        var totals = _generations.Sum(x => x.Total);
+
+        if (totals < _options.MinimumThreshold) return false;
+
+        return (((double)failures) / ((double)totals) >= _ratio);
+    }
+
+    private Task processExceptionsAsync(object[] tokens)
+    {
+        return ProcessExceptionsAsync(DateTimeOffset.UtcNow, tokens).AsTask();
+    }
+
+    public ValueTask ProcessExceptionsAsync(DateTimeOffset time, object[] tokens)
+    {
+        var failures = tokens.OfType<Exception>().Count(x => _match.Matches(x));
+
+        return UpdateTotalsAsync(time, failures, tokens.Length);
+    }
+
+    public async ValueTask UpdateTotalsAsync(DateTimeOffset time, int failures, int total)
+    {
+        var generation = DetermineGeneration(time);
+        generation.Failures += failures;
+        generation.Total += total;
+
+        if (failures > 0 && ShouldStopProcessing())
+        {
+            await _listeningAgent.PauseAsync(_options.PauseTime);
+        }
+    }
+
+    public Generation DetermineGeneration(DateTimeOffset now)
+    {
+        _generations.RemoveAll(x => x.IsExpired(now));
+        if (!_generations.Any(x => x.IsActive(now)))
+        {
+            var generation = new Generation(now, this);
+            _generations.Add(generation);
+
+            return generation;
+        }
+
+        return _generations.Last();
+    }
+
+    public IReadOnlyList<Generation> CurrentGenerations => _generations;
+
+
+    internal class Generation
+    {
+        public bool IsExpired(DateTimeOffset now)
+        {
+            return now > Expires;
+        }
+
+        public bool IsActive(DateTimeOffset now)
+        {
+            return now >= Start && now < End;
+        }
+
+        public DateTimeOffset Start { get; }
+        public DateTimeOffset Expires { get; }
+
+        public Generation(DateTimeOffset start, CircuitBreaker parent)
+        {
+            Start = start;
+            Expires = start.Add(parent._options.TrackingPeriod);
+            End = start.Add(parent.GenerationPeriod);
+        }
+
+        public int Failures { get; set; }
+        public int Total { get; set; }
+        public DateTimeOffset End { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Start)}: {Start}, {nameof(Expires)}: {Expires}, {nameof(End)}: {End}";
+        }
+    }
+
+    public void Dispose()
+    {
+        _cancellation.Cancel();
+        _processingBlock.Complete();
+        _batching.Dispose();
+    }
+}
---
+++ src/Jasper/ErrorHandling/CircuitBreakerOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Baseline.Dates;
+
+namespace Jasper.ErrorHandling;
+
+public class CircuitBreakerOptions
+{
+    public int MinimumThreshold { get; set; } = 10;
+    public int FailurePercentageThreshold { get; set; } = 15;
+    public TimeSpan PauseTime { get; set; } = 5.Minutes();
+    public TimeSpan TrackingPeriod { get; set; } = 10.Minutes();
+
+    public TimeSpan SamplingPeriod { get; set; } = 250.Milliseconds();
+
+    internal void AssertValid()
+    {
+        var messages = validate().ToArray();
+        if (messages.Any())
+        {
+            throw new InvalidCircuitBreakerException(messages);
+        }
+    }
+
+    private IEnumerable<string> validate()
+    {
+        if (MinimumThreshold < 0) yield return $"{nameof(MinimumThreshold)} must be greater than 0";
+
+        if (FailurePercentageThreshold <= 5) yield return $"{nameof(FailurePercentageThreshold)} must be at least 5";
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        if (PauseTime == null) yield return $"{nameof(PauseTime)} cannot be null";
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        if (TrackingPeriod == null) yield return $"{nameof(TrackingPeriod)} cannot be null";
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        if (SamplingPeriod == null) yield return $"{nameof(SamplingPeriod)} cannot be null";
+    }
+
+
+}
