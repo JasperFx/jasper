e9d9ea96ad69b3125883042ffa8785f6c0858df6
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-09-13 08:39:42 -0500

    Upgraded the interop tests to MassTransit 8

--- src/interoperability/MassTransitService/Startup.cs
+++ src/interoperability/MassTransitService/Startup.cs
@@ -31,7 +31,6 @@ namespace MassTransitService
                     });
                 });
             });
-            services.AddMassTransitHostedService(true);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
